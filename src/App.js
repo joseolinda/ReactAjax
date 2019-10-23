7cf43844b9f722209065e9046e9a9fd1d8cc353d
José Olinda <joseolindad@yahoo.com.br>
2019-10-23 15:29:37 -0300

    Add project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MyAjax from './components/MyAjax';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <MyAjax></MyAjax>
+      </div>
+    );
+  }
+}
